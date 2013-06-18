85aa4d8f404620c44afaa46f9da1784bd1cc1f2b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-18 21:48:53 +0200

    Moving to the right arborescence tree

---
+++ src/main/scala/hello/hello.scala
@@ -0,0 +1,15 @@
+package hello;
+
+object Hi {
+  def main(args: Array[String]) :Unit = {
+    val user = "tony";
+    println("Hello " + user + "!");
+
+    val m = max(1,2);
+    println("the max is:" + m);
+  }
+
+  def max(x :Int, y :Int) :Int = {
+    if (x > y) x else y
+  }
+}
