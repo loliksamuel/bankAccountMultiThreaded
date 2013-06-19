c0e7457879603e91096a069e1d04b179d27ae600
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-19 20:19:52 +0200

    Formatting

--- src/main/scala/hello/hello.scala
+++ src/main/scala/hello/hello.scala
@@ -1,15 +1,15 @@
 package hello;
 
 object Hi {
-  def main(args: Array[String]) :Unit = {
+  def main(args: Array[String]): Unit = {
     if (args.length > 0)
       println("Hello " + args(0) + "!")
 
-    val m = max(1,2)
+    val m = max(1, 2)
     println("the max is:" + m)
   }
 
-  def max(x :Int, y :Int) :Int = {
+  def max(x: Int, y: Int): Int = {
     if (x > y) x else y
   }
 }
