afd2a6b6bc938b37a034c5a62477a90286cf9923
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-19 18:06:00 +0200

    No need for ;

--- src/main/scala/hello/hello.scala
+++ src/main/scala/hello/hello.scala
@@ -3,10 +3,10 @@ package hello;
 object Hi {
   def main(args: Array[String]) :Unit = {
     if (args.length > 0)
-      println("Hello " + args(0) + "!");
+      println("Hello " + args(0) + "!")
 
-    val m = max(1,2);
-    println("the max is:" + m);
+    val m = max(1,2)
+    println("the max is:" + m)
   }
 
   def max(x :Int, y :Int) :Int = {
