bca1cd5c3b43de2803e4591a903d09199823917a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-22 18:44:44 +0200

    fixedPoint

---
+++ src/main/scala/coursera/w32.scala
@@ -0,0 +1,26 @@
+package coursera
+
+import math.abs
+
+object w32 {
+  val tolerance = 0.0001
+
+  def isCloseEnough(x: Double, y: Double): Boolean =
+    abs((x - y) / x) / x < tolerance
+
+  def fixedPoint(f: Double => Double) (firstGuess: Double) = {
+    def iterate(guess: Double): Double = {
+      val next = f(guess)
+      if(isCloseEnough(guess, next)) next else iterate(next)
+    }
+    iterate(firstGuess)
+  }
+
+  def main(args: Array[String]) = {
+    println(fixedPoint(x => 1 + x/2)(1))
+  }
+}
+
+// [info] Running coursera.w32
+// 1.999755859375
+// [success] Total time: 3 s, completed 22 juil. 2013 18:44:11
