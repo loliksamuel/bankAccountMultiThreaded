d1532bd32d4ba5e598c870488e329129b9d5e601
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-23 14:55:23 +0200

    Creating singleton

--- src/main/scala/coursera/ConsList.scala
+++ src/main/scala/coursera/ConsList.scala
@@ -4,14 +4,29 @@ trait List[T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+
+  def toString: String
 }
 
 class Nil[T] extends List[T] {
   def isEmpty: Boolean = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+
+  override def toString: String = "."
 }
 
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty: Boolean = false
+
+  override def toString: String = "(" + head + ", " + tail.toString + ")"
+}
+
+object ConsListSession {
+  def main(args: Array[String]) = {
+    def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+
+    println(singleton[Int](1))
+    println(singleton[Boolean](true))
+  }
 }
