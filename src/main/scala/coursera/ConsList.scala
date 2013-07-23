7c4819c21bc3dafbdb412804acccb5b2c9ffb0e9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-23 15:14:33 +0200

    Write a function nth that takes an integer n and a list and selects the nth elements of the lists. Elements are numbered from 0. If index is outside the range from 0 up to the length of the list minus one, an IndexOutOfBoundsException should be thrown.

--- src/main/scala/coursera/ConsList.scala
+++ src/main/scala/coursera/ConsList.scala
@@ -26,7 +26,41 @@ object ConsListSession {
   def main(args: Array[String]) = {
     def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
 
+    def nth[T](n: Int, l: List[T]): T =
+      if (n < 0 || l.isEmpty) throw new IndexOutOfBoundsException("Out of range")
+      else if (n == 0) l.head
+      else nth(n-1, l.tail)
+
+    def catchAndPrint[T](index: Int, l: List[T]) =
+      try {
+        nth(index, l)
+      } catch {
+        case e: IndexOutOfBoundsException => println("exception caught as expected: " + e)
+      }
+
     println(singleton[Int](1))
     println(singleton[Boolean](true))
+
+    val listTypes = new Cons[Int](1, new Cons[Int](2, new Nil[Int]))
+    println(listTypes)
+
+    println(nth(0, listTypes))
+    println(nth(1, listTypes))
+
+    catchAndPrint[Int](-1, listTypes)
+    catchAndPrint[Int](3, listTypes)
+
+    println("end")
   }
 }
+
+// [info] Running coursera.ConsListSession
+// (1, .)
+// (true, .)
+// (1, (2, .))
+// 1
+// 2
+// exception caught as expected: java.lang.IndexOutOfBoundsException: Out of range
+// exception caught as expected: java.lang.IndexOutOfBoundsException: Out of range
+// end
+// [success] Total time: 2 s, completed 23 juil. 2013 15:15:15
