007e7167e21ad8598355fe92c9e1342ef7978b5e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-23 14:43:42 +0200

    Adding implementation to the cons-cell

--- src/main/scala/coursera/ConsList.scala
+++ src/main/scala/coursera/ConsList.scala
@@ -1,13 +1,17 @@
 package coursera
 
 trait List[T] {
-
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
 }
 
 class Nil[T] extends List[T] {
-
+  def isEmpty = true
+  def head = throw new NoSuchElementException("Nil.head")
+  def tail = throw new NoSuchElementException("Nil.tail")
 }
 
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
-
+  def isEmpty = false
 }
