e7a78c5dc96e582a03220cb7198726d530f03d90
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-23 09:53:03 +0200

    Defining add as a method

--- src/main/scala/coursera/Rational.scala
+++ src/main/scala/coursera/Rational.scala
@@ -4,23 +4,23 @@ class Rational(x: Int, y: Int) {
   def numer = x
   def denom = y
 
+  def add(r: Rational): Rational =
+    new Rational(r.numer * denom + numer * r.denom, r.denom * denom)
+
   def makeString(r: Rational): String = r.numer + "/" + r.denom
 
   override def toString(): String = makeString(this)
 }
 
 object Rational {
-  def addRational(r: Rational, s: Rational): Rational =
-    new Rational(r.numer * s.denom + s.numer * r.denom, r.denom * s.denom)
-
   def main(args: Array[String]) = {
     val r0 = new Rational(1, 2)
     val r1 = new Rational(2, 3)
 
-    println(addRational(r0, r1))
+    println(r0.add(r1))
   }
 }
 
 // [info] Running coursera.Rational
 // 7/6
-// [success] Total time: 2 s, completed 23 juil. 2013 09:46:35
+// [success] Total time: 2 s, completed 23 juil. 2013 09:52:37
