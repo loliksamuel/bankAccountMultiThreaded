e1f29699b7c3d4adc07550d77207b4a790299dd4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-23 09:46:54 +0200

    addRational

--- src/main/scala/coursera/Rational.scala
+++ src/main/scala/coursera/Rational.scala
@@ -4,13 +4,23 @@ class Rational(x: Int, y: Int) {
   def numer = x
   def denom = y
 
-  override def toString(): String = "{n: " + numer + ", d: " + denom + "}"
+  def makeString(r: Rational): String = r.numer + "/" + r.denom
+
+  override def toString(): String = makeString(this)
 }
 
 object Rational {
-  def main(args: Array[String]) = {
-    val r = new Rational(1, 2)
+  def addRational(r: Rational, s: Rational): Rational =
+    new Rational(r.numer * s.denom + s.numer * r.denom, r.denom * s.denom)
 
-    println(r)
+  def main(args: Array[String]) = {
+    val r0 = new Rational(1, 2)
+    val r1 = new Rational(2, 3)
+
+    println(addRational(r0, r1))
   }
 }
+
+// [info] Running coursera.Rational
+// 7/6
+// [success] Total time: 2 s, completed 23 juil. 2013 09:46:35
