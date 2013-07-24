5b2f4b1762908a96d7f8c1f40965acfd046efefc
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-24 11:21:18 +0200

    Using map

--- src/main/scala/coursera/HigherOrderFunctionList.scala
+++ src/main/scala/coursera/HigherOrderFunctionList.scala
@@ -1,9 +1,6 @@
 object sessionHOFList {
   def main(args: Array[String]) = {
-    def scaleList(xs: List[Double], factor: Double): List[Double] = xs match {
-      case Nil => xs
-      case y :: ys => y * factor :: scaleList(ys, factor)
-    }
+    def scaleList(xs: List[Double], factor: Double): List[Double] = xs map (x => x * factor)
 
     val elems = List(1.0, 1.5, 2.9, 4.5)
     println(scaleList(elems, 0.5))
@@ -12,4 +9,4 @@ object sessionHOFList {
 
 // [info] Running sessionHOFList
 // List(0.5, 0.75, 1.45, 2.25)
-// [success] Total time: 3 s, completed 24 juil. 2013 11:17:53
+// [success] Total time: 3 s, completed 24 juil. 2013 11:20:42
