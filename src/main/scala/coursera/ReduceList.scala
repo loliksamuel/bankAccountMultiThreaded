d695686694e510454df8ddcc6ba023b536d0ae69
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-24 12:28:55 +0200

    Syntactic sugar around anonymous function

--- src/main/scala/coursera/ReduceList.scala
+++ src/main/scala/coursera/ReduceList.scala
@@ -1,12 +1,12 @@
 object sessionReduceList {
   def main(args: Array[String]) = {
-    def sumList(xs: List[Int]): Int = (0 :: xs) reduceLeft ((x, y) => x + y)
+    def sumList(xs: List[Int]): Int = (0 :: xs) reduceLeft (_ + _)
 
     val elems = List(1, 3, 5, 7)
     println(sumList(elems))
   }
 }
 
-// [info] Running sessionReduceList
+// [info] Running sessionReduceList 1
 // 16
-// [success] Total time: 1 s, completed 24 juil. 2013 12:26:25
+// [success] Total time: 1 s, completed 24 juil. 2013 12:28:35
