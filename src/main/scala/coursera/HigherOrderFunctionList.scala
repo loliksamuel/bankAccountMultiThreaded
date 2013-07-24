a424fd5d5aea2df5a6a8fbe4e707bd4159498207
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-24 11:22:55 +0200

    No need for intermediary function

--- src/main/scala/coursera/HigherOrderFunctionList.scala
+++ src/main/scala/coursera/HigherOrderFunctionList.scala
@@ -1,12 +1,12 @@
 object sessionHOFList {
   def main(args: Array[String]) = {
-    def scaleList(xs: List[Double], factor: Double): List[Double] = xs map (x => x * factor)
-
     val elems = List(1.0, 1.5, 2.9, 4.5)
-    println(scaleList(elems, 0.5))
+    val elemsMap = elems map (x => x * 0.5)
+
+    println(elemsMap)
   }
 }
 
 // [info] Running sessionHOFList
 // List(0.5, 0.75, 1.45, 2.25)
-// [success] Total time: 3 s, completed 24 juil. 2013 11:20:42
+// [success] Total time: 6 s, completed 24 juil. 2013 11:22:39
