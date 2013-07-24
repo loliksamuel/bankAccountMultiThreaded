f8b86930bb0eb6541a0d3b75811e70a80267283a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-24 11:32:53 +0200

    filtering positive elements without filter

--- src/main/scala/coursera/HigherOrderFunctionList.scala
+++ src/main/scala/coursera/HigherOrderFunctionList.scala
@@ -1,15 +1,26 @@
 object sessionHOFList {
   def main(args: Array[String]) = {
+
+    // filter only elements that are strictly positive
+    def posElems(xs: List[Int]): List[Int] = xs match {
+        case Nil     => Nil
+        case y :: ys => if (y > 0) y :: posElems(ys)
+                        else       posElems(ys)
+      }
+
     val elems = List(1.0, 1.5, 2.9, 4.5)
     println(elems map (x => x * 0.5))
 
     val ilems = List(1, 3, 5, 7)
     println(ilems map (x => x * x))
+
+    val nlems = List(-1, 1, 10, -2, 45)
+    println(posElems(nlems))
   }
 }
 
 // [info] Running sessionHOFList
 // List(0.5, 0.75, 1.45, 2.25)
 // List(1, 9, 25, 49)
-// List(1, 9, 25, 49)
-// [success] Total time: 3 s, completed 24 juil. 2013 11:26:25
+// List(1, 10, 45)
+// [success] Total time: 2 s, completed 24 juil. 2013 11:32:34
