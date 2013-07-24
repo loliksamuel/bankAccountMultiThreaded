68d3c6d33215df312b455cc8639192ee14d1ab29
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-24 09:49:25 +0200

    Generify msort function

--- src/main/scala/coursera/PairTuple.scala
+++ src/main/scala/coursera/PairTuple.scala
@@ -1,28 +1,29 @@
-//import scala.util._
-
 object sessionPairTuple {
-  def msort(xs: List[Int]): List[Int] = {
+  def msort[T](xs: List[T])(lt: (T, T) => Boolean): List[T] = {
     val n = xs.length / 2
       if (n == 0) xs
       else {
-        def merge(xs: List[Int], ys: List[Int]): List[Int] = (xs, ys) match {
+        def merge(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
             case (Nil, ys) => ys
             case (xs, Nil) => xs
-            case (x :: xss, y :: yss) => if (x < y) x :: merge(xss, ys)
-                                         else       y :: merge(xs, yss)
+            case (x :: xss, y :: yss) => if (lt(x,y)) x :: merge(xss, ys)
+                                         else         y :: merge(xs, yss)
         }
         val (f, s) = xs splitAt n
-        merge(msort(f), msort(s))
+        merge(msort(f)(lt), msort(s)(lt))
       }
   }
 
   def main(args: Array[String]) = {
     val elems = List(10, 8 , 3, 1, 20)
+    println(msort(elems)((x,y) => x < y))
 
-    println(msort(elems))
+    val fruits = List("apple", "pineapple", "orange", "banana")
+    println(msort(fruits)((x,y) => x.compareTo(y) < 0))
   }
 }
 
 // [info] Running sessionPairTuple
 // List(1, 3, 8, 10, 20)
-// [success] Total time: 6 s, completed 24 juil. 2013 09:24:51
+// List(apple, banana, orange, pineapple)
+// [success] Total time: 5 s, completed 24 juil. 2013 09:51:49
