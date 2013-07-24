b3ca2ade4d76498c545e017aa5f989d1714a9bdd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-24 12:32:12 +0200

    Using foldLeft which takes the initial value for the accumulator

--- src/main/scala/coursera/ReduceList.scala
+++ src/main/scala/coursera/ReduceList.scala
@@ -1,7 +1,7 @@
 object sessionReduceList {
   def main(args: Array[String]) = {
-    def sum(xs: List[Int]): Int = (0 :: xs) reduceLeft (_ + _)
-    def pdt(xs: List[Int]): Int = (1 :: xs) reduceLeft (_ * _)
+    def sum(xs: List[Int]): Int = (xs foldLeft 0) (_ + _)
+    def pdt(xs: List[Int]): Int = (xs foldLeft 1) (_ * _)
 
     val elems = List(1, 3, 5, 7)
     println(sum(elems))
@@ -12,4 +12,4 @@ object sessionReduceList {
 // [info] Running sessionReduceList
 // 16
 // 105
-// [success] Total time: 0 s, completed 24 juil. 2013 12:29:44
+// [success] Total time: 1 s, completed 24 juil. 2013 12:31:35
