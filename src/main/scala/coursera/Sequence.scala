b7639b98bbc8c67c6bab5fb7243e4c8540c780d7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-25 15:24:12 +0200

    basic manipulation on sets

--- src/main/scala/coursera/Sequence.scala
+++ src/main/scala/coursera/Sequence.scala
@@ -2,10 +2,16 @@ package coursera
 
 object sessionSequence {
   def main(args: Array[String]) = {
+    val fruit = Set("apple", "banana", "pear")
+    val s = (1 to 6).toSet
+
     println(1 until 5)
     println(1 to 5)
     println(1 to 10 by 3)
     println(6 to 1 by -2)
+
+    println(fruit)
+    println(s)
   }
 
 }
@@ -15,4 +21,5 @@ object sessionSequence {
 // Range(1, 2, 3, 4, 5)
 // Range(1, 4, 7, 10)
 // Range(6, 4, 2)
-// [success] Total time: 6 s, completed 25 juil. 2013 14:29:56
+// Set(apple, banana, pear)
+// Set(5, 1, 6, 2, 3, 4)
