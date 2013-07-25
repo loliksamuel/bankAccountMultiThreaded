3d4253d2c32a3033db2c8b4c40fd775f9e28cbd5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-25 12:09:41 +0200

    Using pattern matching

--- src/main/scala/coursera/Expressions.scala
+++ src/main/scala/coursera/Expressions.scala
@@ -1,25 +1,22 @@
 package coursera
 
-trait Expr {
-  def eval: Int
-}
+trait Expr
 
-class Number(n: Int) extends Expr {
-  def eval: Int = n
-}
-
-class Sum(e1: Expr, e2: Expr) extends Expr {
-  def eval: Int = e1.eval + e2.eval
-}
-
-class Pdt(e1: Expr, e2: Expr) extends Expr {
-  def eval: Int = e1.eval * e2.eval
-}
+case class Number(n: Int) extends Expr
+case class Sum(e1: Expr, e2: Expr) extends Expr
+case class Pdt(e1: Expr, e2: Expr) extends Expr
 
 object sessionExpressions {
+
+  def eval(e: Expr): Int = e match {
+      case Number(n)   => n
+      case Sum(e1, e2) => eval(e1) + eval(e2)
+      case Pdt(e1, e2) => eval(e1) * eval(e2)
+  }
+
   def main(args: Array[String]) = {
-    println(new Sum(new Number(10), new Number(-5)).eval)
-    println(new Pdt(new Sum(new Number(10), new Number(-5)), new Number(2)).eval)
+    println(eval(new Sum(new Number(10), new Number(-5))))
+    println(eval(new Pdt(new Sum(new Number(10), new Number(-5)), new Number(2))))
   }
 
 }
@@ -27,4 +24,4 @@ object sessionExpressions {
 // [info] Running coursera.sessionExpressions
 // 5
 // 10
-// [success] Total time: 2 s, completed 25 juil. 2013 12:00:08
+// [success] Total time: 2 s, completed 25 juil. 2013 12:09:00
