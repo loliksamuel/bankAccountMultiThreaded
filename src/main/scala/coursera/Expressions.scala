9fa7c2bc701fc0301ccd15f04fc88ba5af243e6e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-25 12:00:29 +0200

    OO decomposition

--- src/main/scala/coursera/Expressions.scala
+++ src/main/scala/coursera/Expressions.scala
@@ -1,42 +1,30 @@
 package coursera
 
 trait Expr {
-  def isNumber: Boolean
-  def isSum: Boolean
-  def numValue: Int
-  def leftOp: Expr
-  def rightOp: Expr
+  def eval: Int
 }
 
 class Number(n: Int) extends Expr {
-  def isNumber: Boolean = true
-  def isSum: Boolean = false
-  def numValue: Int = n
-  def leftOp: Expr = throw new Error("Number.leftOp")
-  def rightOp: Expr = throw new Error("Number.rightOp")
+  def eval: Int = n
 }
 
 class Sum(e1: Expr, e2: Expr) extends Expr {
-  def isNumber: Boolean = false
-  def isSum: Boolean = true
-  def numValue: Int = throw new Error("Sum.numValue")
-  def leftOp: Expr = e1
-  def rightOp: Expr = e2
+  def eval: Int = e1.eval + e2.eval
+}
+
+class Pdt(e1: Expr, e2: Expr) extends Expr {
+  def eval: Int = e1.eval * e2.eval
 }
 
 object sessionExpressions {
   def main(args: Array[String]) = {
-    def eval(e: Expr): Int = {
-      if(e.isNumber) e.numValue
-      else if(e.isSum) eval(e.leftOp) + eval(e.rightOp)
-      else throw new Error("Unknown Expression " + e)
-    }
-
-    println(eval(new Sum(new Number(10), new Number(-5))))
+    println(new Sum(new Number(10), new Number(-5)).eval)
+    println(new Pdt(new Sum(new Number(10), new Number(-5)), new Number(2)).eval)
   }
 
 }
 
 // [info] Running coursera.sessionExpressions
 // 5
-// [success] Total time: 6 s, completed 25 juil. 2013 11:45:39
+// 10
+// [success] Total time: 2 s, completed 25 juil. 2013 12:00:08
