b0758e45f910c7fe49e89f5573ca8f962d0c0270
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-25 15:17:22 +0200

    Using the list comprehension!

--- src/main/scala/coursera/VectorManipulation.scala
+++ src/main/scala/coursera/VectorManipulation.scala
@@ -14,7 +14,10 @@ object sessionVectorManipulation {
       (2 until n) forall (x => n % x != 0)
 
     def pairsPrime(n: Int) = {
-      (1 until n) flatMap (i => (1 until i) map (j => (i, j))) filter { case (x, y) => isPrime(x + y) }
+      for { i <- 1 until n
+            j <- 1 until i
+            if isPrime(i + j)
+      } yield (i, j)
     }
 
     val nums = Vector(1, 2, 3, -88)
@@ -41,4 +44,4 @@ object sessionVectorManipulation {
 // 20
 // Vector(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
 // Vector((2,1), (3,2), (4,1), (4,3), (5,2), (6,1), (6,5), (7,4), (7,6), (8,3), (8,5), (9,2), (9,4), (9,8))
-// [success] Total time: 1 s, completed 25 juil. 2013 15:11:35
+// [success] Total time: 2 s, completed 25 juil. 2013 15:16:56
