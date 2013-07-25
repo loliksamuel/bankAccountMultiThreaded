2bf2e99ffe4418042e77f8b25eb778e7b4122d3f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-25 14:38:40 +0200

    zip

--- src/main/scala/coursera/VectorManipulation.scala
+++ src/main/scala/coursera/VectorManipulation.scala
@@ -5,11 +5,18 @@ object sessionVectorManipulation {
   def main(args: Array[String]) = {
     val nums = Vector(1, 2, 3, -88)
 
+    val names = Vector("one", "two", "three", "minus eighty eight")
+
     println(nums)
+    println(names)
+
+    println(names zip nums)
   }
 
 }
 
 // [info] Running coursera.sessionVectorManipulation
 // Vector(1, 2, 3, -88)
-// [success] Total time: 3 s, completed 25 juil. 2013 14:21:45
+// Vector(one, two, three, minus eighty eight)
+// Vector((one,1), (two,2), (three,3), (minus eighty eight,-88))
+// [success] Total time: 23 s, completed 25 juil. 2013 14:38:22
