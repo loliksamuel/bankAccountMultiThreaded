4ef671043efa0eedfe5c00cf2be6d37bdbc944ea
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-25 15:12:08 +0200

    List pairs that are prime

--- src/main/scala/coursera/VectorManipulation.scala
+++ src/main/scala/coursera/VectorManipulation.scala
@@ -13,6 +13,10 @@ object sessionVectorManipulation {
     def isPrime(n: Int): Boolean =
       (2 until n) forall (x => n % x != 0)
 
+    def pairsPrime(n: Int) = {
+      (1 until n) flatMap (i => (1 until i) map (j => (i, j))) filter { case (x, y) => isPrime(x + y) }
+    }
+
     val nums = Vector(1, 2, 3, -88)
     val names = Vector("one", "two", "three", "minus eighty eight")
 
@@ -22,10 +26,9 @@ object sessionVectorManipulation {
     println(names)
     println(names zip nums)
     println(combi(2, 10))
-
     println(scalarPdt(nums, nums2))
-
     println((2 to 100) filter isPrime)
+    println(pairsPrime(10))
   }
 
 }
@@ -37,4 +40,5 @@ object sessionVectorManipulation {
 // Vector((1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8), (1,9), (1,10), (2,1), (2,2), (2,3), (2,4), (2,5), (2,6), (2,7), (2,8), (2,9), (2,10))
 // 20
 // Vector(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
-// [success] Total time: 1 s, completed 25 juil. 2013 14:58:20
+// Vector((2,1), (3,2), (4,1), (4,3), (5,2), (6,1), (6,5), (7,4), (7,6), (8,3), (8,5), (9,2), (9,4), (9,8))
+// [success] Total time: 1 s, completed 25 juil. 2013 15:11:35
