a9ac5c3409eff9601338d9aaf896808265accec4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-25 14:21:58 +0200

    basic vector manipulation

---
+++ src/main/scala/coursera/VectorManipulation.scala
@@ -0,0 +1,15 @@
+package coursera
+
+object sessionVectorManipulation {
+
+  def main(args: Array[String]) = {
+    val nums = Vector(1, 2, 3, -88)
+
+    println(nums)
+  }
+
+}
+
+// [info] Running coursera.sessionVectorManipulation
+// Vector(1, 2, 3, -88)
+// [success] Total time: 3 s, completed 25 juil. 2013 14:21:45
