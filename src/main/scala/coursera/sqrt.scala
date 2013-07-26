397d952c7f32944596f88530b9bd19927d5a88f2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-26 10:53:47 +0200

    square root using stream

--- src/main/scala/coursera/sqrt.scala
+++ src/main/scala/coursera/sqrt.scala
@@ -4,6 +4,15 @@ import math.abs
 
 object sessionSqrt {
 
+  def sqrtStream(x: Double): Stream[Double] = {
+    def improve(guess: Double) = (guess + x / guess) / 2
+    lazy val guesses: Stream[Double] = 1 #:: (guesses map improve)
+    guesses
+  }
+
+  def isGoodEnough(guess: Double, x: Double): Boolean =
+    abs((guess * guess - x) / x) < 0.0001
+
   def sqrt(x: Double) = FixedPointCompute.fixedPoint(y => x/y)(1.0)
 
   def main(args: Array[String]) = {
@@ -12,10 +21,13 @@ object sessionSqrt {
     val squareValues = values.map(v => List(v, sqrt(v)))
 
     squareValues.foreach(println)
+
+    println(sqrtStream(4) take(10) toList)
+    println(sqrtStream(4) filter(isGoodEnough(_, 4)) take(10) toList)
   }
 }
 
-// [info] Running coursera.w1
+// [info] Running coursera.sessionSqrt
 // List(2.0, 1.4142135623746899)
 // List(4.0, 2.000000000000002)
 // List(1.0E-6, 0.001)
@@ -23,4 +35,6 @@ object sessionSqrt {
 // List(1.0E-21, 3.162277660168379E-11)
 // List(1.0E20, 2.5E19)
 // List(1.0E50, 2.5E49)
-// [success] Total time: 2 s, completed 22 juil. 2013 18:52:31
+// List(1.0, 2.5, 2.05, 2.000609756097561, 2.0000000929222947, 2.000000000000002, 2.0, 2.0, 2.0, 2.0)
+// List(2.0000000929222947, 2.000000000000002, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0)
+// [success] Total time: 1 s, completed 26 juil. 2013 10:53:15
