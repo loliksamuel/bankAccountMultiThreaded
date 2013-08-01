13143e9d519090c84c185db4181763e3c9edc6dd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-08-01 16:41:23 +0200

    Using generic type!

--- src/test/scala/p99/99.scala
+++ src/test/scala/p99/99.scala
@@ -4,14 +4,14 @@ import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
 object ex99Spec extends Properties("ex99") {
-  property("last") = forAll { (l: List[Int], x: Int) =>
+  property("last") = forAll { (l: List[AnyVal], x: AnyVal) =>
       l match {
         case Nil    => ex99.last(x :: l) == x
         case _ :: _ => ex99.last(l) == ex99.last(x :: l)
       }
     }
 
-  property("penultimate") = forAll { (l: List[Int], x: Int, y: Int) =>
+  property("penultimate") = forAll { (l: List[AnyVal], x: AnyVal, y: AnyVal) =>
       l match {
         case Nil      => ex99.penultimate(x :: y :: Nil) == x
         case z :: Nil => ex99.penultimate(x :: z :: Nil) == x
