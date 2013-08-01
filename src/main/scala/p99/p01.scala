2533ac746524ff3f8ce2f0216edfe1269dc099ac
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-08-01 16:12:49 +0200

    Standalone test with a more thoughout approach

--- src/main/scala/p99/p01.scala
+++ src/main/scala/p99/p01.scala
@@ -2,13 +2,13 @@ package p99
 
 object p01 {
   def last[T](l: List[T]): T = l match {
-      case Nil      => throw new Error ("List.last on Nil")
+      case Nil      => throw new NoSuchElementException("Nil.last")
       case x :: Nil => x
-      case x :: xs  => last(xs)
+      case _ :: xs  => last(xs)
     }
 
   def main(args: Array[String]) = {
-
     println(last(List(1, 2, 3, 4)))
+    println(last(List("1", "2", "3", "4")))
   }
 }
--- src/test/scala/p99/p01.scala
+++ src/test/scala/p99/p01.scala
@@ -4,10 +4,10 @@ import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
 object p01Spec extends Properties("p01") {
-  property("last") = forAll {
-      l: List[Int] => l match {
-        case Nil     => true
-        case x :: xs => p01.last(l) == l.last
+  property("last") = forAll { (l: List[Int], x: Int) =>
+      l match {
+        case Nil    => p01.last(x :: l) == x
+        case _ :: _ => p01.last(l) == p01.last(x :: l)
       }
     }
 }
