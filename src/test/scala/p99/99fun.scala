75d999f1f45343395cf39e4dcb7bd7859435d1fd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-29 11:18:48 +0200

    formatting

--- src/test/scala/p99/99fun.scala
+++ src/test/scala/p99/99fun.scala
@@ -2,39 +2,38 @@ package p99
 
 import org.scalatest.FunSuite
 
-
 class ex99FunTests extends FunSuite {
-  test("compress") {
+  test("compress.") {
     assert(ex99.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List('a,'b,'c,'d,'e))
     assert(ex99.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List('a,'b,'c,'a,'d,'e))
   }
 
-  test("pack") {
+  test("pack.") {
     assert(ex99.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('d), List('e, 'e, 'e, 'e)))
   }
 
-  test("encode - run-length encoding") {
+  test("encode - run-length encoding.") {
     assert(ex99.encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e)))
   }
 
-  test("encode - run-length encoding modified") {
+  test("encode - run-length encoding modified.") {
     assert(ex99.runLengthEncodingModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), 'b, (2, 'c), 'd, (4, 'e)))
   }
 
-  test("decode - run-length encoding") {
+  test("decode - run-length encoding.") {
     assert(ex99.decode(List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e))) == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e))
   }
 
-  test("encode1 - direct run-length encoding") {
+  test("encode1 - direct run-length encoding.") {
     assert(ex99.encode1(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e)))
   }
 
-  test("duplicate element in double") {
+  test("duplicate element in double.") {
     assert(ex99.duplicate(List(1, 2, 3, 4)) == List(1, 1, 2, 2, 3, 3, 4, 4))
     assert(ex99.duplicate(List(3, 2)) == List(3, 3, 2, 2))
   }
 
-  test("duplicate element in a given amount of time double") {
+  test("duplicate element in a given amount of time.") {
     assert(ex99.duplicateN(3, List(1, 2, 3, 4)) == List(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4))
     assert(ex99.duplicateN(5, List(3, 2)) == List(3, 3, 3, 3, 3, 2, 2, 2, 2, 2))
   }
