0d60979d112566bb25813574b1a597ad379f8500
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-12 20:52:34 +0200

    Formatting

--- src/test/scala/p99/99.scala
+++ src/test/scala/p99/99.scala
@@ -3,26 +3,27 @@ package p99
 import org.scalacheck._
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
+import p99.ex99._
 
 object ex99ScalaCheck extends Properties("ex99") {
   property("last") = forAll { (l: List[AnyVal], x: AnyVal) =>
       l match {
-        case Nil    => ex99.last(x :: l) == x
-        case _ :: _ => ex99.last(l) == ex99.last(x :: l)
+        case Nil    => last(x :: l) == x
+        case _ :: _ => last(l) == last(x :: l)
       }
     }
 
   property("penultimate") = forAll { (l: List[AnyVal], x: AnyVal, y: AnyVal) =>
       l match {
-        case Nil      => ex99.penultimate(x :: y :: Nil) == x
-        case z :: Nil => ex99.penultimate(x :: z :: Nil) == x
-        case _ :: _   => ex99.penultimate(l) == ex99.penultimate(x :: l)
+        case Nil      => penultimate(x :: y :: Nil) == x
+        case z :: Nil => penultimate(x :: z :: Nil) == x
+        case _ :: _   => penultimate(l) == penultimate(x :: l)
       }
     }
 
   property("nth") = forAll { (l: List[AnyVal], n: Int, x: AnyVal) =>
       try {
-        ex99.nth(n, l) == ex99.nth(n+1, x :: l)
+        nth(n, l) == nth(n+1, x :: l)
       } catch {
         case e: NoSuchElementException    => true // expected
         case _: Throwable                 => false // absolutely not expected
@@ -30,28 +31,28 @@ object ex99ScalaCheck extends Properties("ex99") {
     }
 
   property("length") = forAll { (l: List[AnyVal], x: AnyVal) =>
-    ex99.length(x :: l) == 1 + ex99.length(l)
+    length(x :: l) == 1 + length(l)
     }
 
   property("reverse") = forAll { l: List[AnyVal] =>
-      ex99.reverse(ex99.reverse(l)) == l
+      reverse(reverse(l)) == l
     }
 
   property("palindrome") = forAll { l: List[AnyVal] =>
-      ex99.isPalindrome(l) == (l.reverse == l)
+      isPalindrome(l) == (l.reverse == l)
     }
 
   property("flatten") = forAll { (l: List[List[AnyVal]], x: List[AnyVal]) =>
-      val fl = ex99.flatten(x :: l)
+      val fl = flatten(x :: l)
       x forall (fl contains _)
     }
 
   // property("randomSelect") = forAll { (l: List[List[AnyVal]], k: Int) =>
-  //     ex99.randomSelect(k, l) forall (l contains _)
+  //     randomSelect(k, l) forall (l contains _)
   //   }
 
   // property("lotto") = forAll { (k: Int, end: Int) =>
   //     val l = Range(1, end).toList
-  //     ex99.lotto(k, end) forall (l contains _)
+  //     lotto(k, end) forall (l contains _)
   //   }
 }
--- src/test/scala/p99/99fun.scala
+++ src/test/scala/p99/99fun.scala
@@ -1,6 +1,7 @@
 package p99
 
 import org.scalatest.FunSuite
+import p99.ex99._
 
 class ex99FunTests extends FunSuite {
   def assertion[T](actual: T, expected: T) = {
@@ -14,95 +15,95 @@ class ex99FunTests extends FunSuite {
   }
 
   test("compress.") {
-    assert(ex99.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List('a,'b,'c,'d,'e))
-    assert(ex99.compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List('a,'b,'c,'a,'d,'e))
+    assert(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List('a,'b,'c,'d,'e))
+    assert(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List('a,'b,'c,'a,'d,'e))
   }
 
   test("pack.") {
-    assert(ex99.pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('d), List('e, 'e, 'e, 'e)))
+    assert(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('d), List('e, 'e, 'e, 'e)))
   }
 
   test("encode - run-length encoding.") {
-    assert(ex99.encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e)))
+    assert(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e)))
   }
 
   test("encode - run-length encoding modified.") {
-    assert(ex99.runLengthEncodingModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), 'b, (2, 'c), 'd, (4, 'e)))
+    assert(runLengthEncodingModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), 'b, (2, 'c), 'd, (4, 'e)))
   }
 
   test("decode - run-length encoding.") {
-    assert(ex99.decode(List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e))) == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e))
+    assert(decode(List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e))) == List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e))
   }
 
   test("encode1 - direct run-length encoding.") {
-    assert(ex99.encode1(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e)))
+    assert(encode1(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'd, 'e, 'e, 'e, 'e)) == List((4, 'a), (1, 'b), (2, 'c), (1, 'd), (4, 'e)))
   }
 
   test("duplicate element in double.") {
-    assert(ex99.duplicate(List(1, 2, 3, 4)) == List(1, 1, 2, 2, 3, 3, 4, 4))
-    assert(ex99.duplicate(List(3, 2)) == List(3, 3, 2, 2))
+    assert(duplicate(List(1, 2, 3, 4)) == List(1, 1, 2, 2, 3, 3, 4, 4))
+    assert(duplicate(List(3, 2)) == List(3, 3, 2, 2))
   }
 
   test("duplicate element in a given amount of time.") {
-    assert(ex99.duplicateN(3, List(1, 2, 3, 4)) == List(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4))
-    assert(ex99.duplicateN(5, List(3, 2)) == List(3, 3, 3, 3, 3, 2, 2, 2, 2, 2))
+    assert(duplicateN(3, List(1, 2, 3, 4)) == List(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4))
+    assert(duplicateN(5, List(3, 2)) == List(3, 3, 3, 3, 3, 2, 2, 2, 2, 2))
   }
 
   test("drop every nth element on the list.") {
-    assert(ex99.dropN(3, List(1, 2, 3, 4)) == List(1, 2, 4))
-    assert(ex99.dropN(2, List(3, 2, 1, 0, 4)) == List(3, 1, 4))
+    assert(dropN(3, List(1, 2, 3, 4)) == List(1, 2, 4))
+    assert(dropN(2, List(3, 2, 1, 0, 4)) == List(3, 1, 4))
   }
 
   test("split a list at a given position into 2 lists.") {
-    assert(ex99.splitN(3, List(1, 2, 3, 4)) == (List(1, 2, 3), List(4)))
-    assert(ex99.splitN(2, List(3, 2, 1, 0, 4)) == (List(3, 2), List(1, 0, 4)))
+    assert(splitN(3, List(1, 2, 3, 4)) == (List(1, 2, 3), List(4)))
+    assert(splitN(2, List(3, 2, 1, 0, 4)) == (List(3, 2), List(1, 0, 4)))
   }
 
   test("slice") {
-    assert(ex99.slice(0, 3, List(1, 2, 3, 4, 5)) == List(1, 2, 3))
-    assert(ex99.slice(4, 5, List(1, 2, 3, 4, 5, 6, 7)) == List(5))
+    assert(slice(0, 3, List(1, 2, 3, 4, 5)) == List(1, 2, 3))
+    assert(slice(4, 5, List(1, 2, 3, 4, 5, 6, 7)) == List(5))
   }
 
   test("rotate") {
-    assert(ex99.rotate(0, List(1, 2, 3, 4, 5)) == List(1, 2, 3, 4, 5))
-    assert(ex99.rotate(1, List(1, 2, 3, 4, 5)) == List(2, 3, 4, 5, 1))
-    assert(ex99.rotate(2, List(1, 2, 3, 4, 5)) == List(3, 4, 5, 1, 2))
-    assert(ex99.rotate(5, List(1, 2, 3, 4, 5)) == List(1, 2, 3, 4, 5))
-    assert(ex99.rotate(-1, List(1, 2, 3, 4, 5)) == List(5, 1, 2, 3, 4))
+    assert(rotate(0, List(1, 2, 3, 4, 5)) == List(1, 2, 3, 4, 5))
+    assert(rotate(1, List(1, 2, 3, 4, 5)) == List(2, 3, 4, 5, 1))
+    assert(rotate(2, List(1, 2, 3, 4, 5)) == List(3, 4, 5, 1, 2))
+    assert(rotate(5, List(1, 2, 3, 4, 5)) == List(1, 2, 3, 4, 5))
+    assert(rotate(-1, List(1, 2, 3, 4, 5)) == List(5, 1, 2, 3, 4))
   }
 
   test("removeAt") {
-    assert(ex99.removeAt(0, List(1, 2, 3, 4, 5)) == (List(2, 3, 4, 5), Some(1)))
-    assert(ex99.removeAt(1, List(1, 2, 3, 4, 5)) == (List(1, 3, 4, 5), Some(2)))
-    assert(ex99.removeAt(2, List(1, 2, 3, 4, 5)) == (List(1, 2, 4, 5), Some(3)))
-    assert(ex99.removeAt(6, List(1, 2, 3, 4, 5)) == (List(1, 2, 3, 4, 5), None))
+    assert(removeAt(0, List(1, 2, 3, 4, 5)) == (List(2, 3, 4, 5), Some(1)))
+    assert(removeAt(1, List(1, 2, 3, 4, 5)) == (List(1, 3, 4, 5), Some(2)))
+    assert(removeAt(2, List(1, 2, 3, 4, 5)) == (List(1, 2, 4, 5), Some(3)))
+    assert(removeAt(6, List(1, 2, 3, 4, 5)) == (List(1, 2, 3, 4, 5), None))
 
     intercept[NoSuchElementException] {
-      ex99.removeAt(-1, List())
+      removeAt(-1, List())
     }
   }
 
   test("insertAt") {
-    assert(ex99.insertAt(-1, 0, List(1, 2, 3, 4, 5)) == List(-1, 1, 2, 3, 4, 5))
-    assert(ex99.insertAt(-1, 1, List(1, 2, 3, 4, 5)) == List(1, -1, 2, 3, 4, 5))
-    assert(ex99.insertAt('f, 3, List('a, 'b, 'c, 'd, 'e)) == List('a, 'b, 'c, 'f, 'd, 'e))
+    assert(insertAt(-1, 0, List(1, 2, 3, 4, 5)) == List(-1, 1, 2, 3, 4, 5))
+    assert(insertAt(-1, 1, List(1, 2, 3, 4, 5)) == List(1, -1, 2, 3, 4, 5))
+    assert(insertAt('f, 3, List('a, 'b, 'c, 'd, 'e)) == List('a, 'b, 'c, 'f, 'd, 'e))
   }
 
   test("range") {
-    assert(ex99.range(0, 2) == List(0, 1, 2))
-    assert(ex99.range(2, 5) == List(2, 3, 4, 5))
-    assert(ex99.range(5, 5) == List(5))
+    assert(range(0, 2) == List(0, 1, 2))
+    assert(range(2, 5) == List(2, 3, 4, 5))
+    assert(range(5, 5) == List(5))
   }
 
   test("randomSelect") {
-    assert(ex99.randomSelect(3, List(1, 2, 4, 6, 10)).length == 3)
-    assert(ex99.randomSelect(4, List(1, 2, 4, 6, 10)).length == 4)
-    assert(ex99.randomSelect(5, List(1, 2, 4, 6, 10)).length == 5)
+    assert(randomSelect(3, List(1, 2, 4, 6, 10)).length == 3)
+    assert(randomSelect(4, List(1, 2, 4, 6, 10)).length == 4)
+    assert(randomSelect(5, List(1, 2, 4, 6, 10)).length == 5)
   }
 
   test("randomPermute") {
     val list = List(1, 2, 3, 4, 5)
-    assert(ex99.randomPermute(list).toSet == list.toSet)
-    assert(ex99.randomPermute(list).length == list.length)
+    assert(randomPermute(list).toSet == list.toSet)
+    assert(randomPermute(list).length == list.length)
   }
 }
