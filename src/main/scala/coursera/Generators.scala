12456546ea3b91eb1c34d27e74c6ea0aa8204643
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-11-07 18:38:06 +0100

    boilerplate to generate random values

--- src/main/scala/coursera/Generators.scala
+++ src/main/scala/coursera/Generators.scala
@@ -2,7 +2,27 @@ package coursera
 
 import java.util.Random
 
-object Test extends Application {
-  val rand = new Random
-  println(rand.nextInt())
+
+trait Generator[+T] {
+  def generate: T
+}
+
+object Generator {
+  def main(args: List[String]) = {
+    val rand = new Random
+    println(rand.nextInt())
+  }
+
+  val integers = new Generator[Int] {
+      val rand = new Random
+      def generate = rand.nextInt()
+    }
+
+  val booleans = new Generator[Boolean] {
+      def generate = integers.generate > 0
+    }
+
+  val pairs = new Generator[(Int, Int)] {
+      def generate = (integers.generate, integers.generate)
+    }
 }
