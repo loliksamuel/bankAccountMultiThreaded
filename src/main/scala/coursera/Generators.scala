3dbc11603b1b63349c80e624305e929847288ef2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-11-07 19:17:32 +0100

    Generators

--- src/main/scala/coursera/Generators.scala
+++ src/main/scala/coursera/Generators.scala
@@ -2,27 +2,51 @@ package coursera
 
 import java.util.Random
 
-
 trait Generator[+T] {
-  def generate: T
-}
+  self => // an alias for this
 
-object Generator {
-  def main(args: List[String]) = {
-    val rand = new Random
-    println(rand.nextInt())
-  }
+  def generate: T
 
   val integers = new Generator[Int] {
       val rand = new Random
       def generate = rand.nextInt()
     }
 
-  val booleans = new Generator[Boolean] {
-      def generate = integers.generate > 0
+  def map[S](f: T => S): Generator[S] = new Generator[S] {
+      def generate = f(self.generate)
     }
 
-  val pairs = new Generator[(Int, Int)] {
-      def generate = (integers.generate, integers.generate)
+  def flatMap[S](f: T => Generator[S]): Generator[S] = new Generator[S] {
+      def generate = f(self.generate).generate
     }
+
+  def single[T](x: T): Generator[T] = new Generator[T] {
+      def generate = x
+    }
+
+  def choose(lo: Int, hi: Int): Generator[Int] =
+    for(x <- integers) yield lo + x % (hi - lo)
+
+  def oneOf[T](xs: T*): Generator[T] =
+    for(idx <- choose(0, xs.length)) yield xs(idx)
+}
+
+object Generator {
+  def pairs[T, U](t: Generator[T], u:Generator[U]) = new Generator[(T, U)] {
+      def generate = (t.generate, u.generate)
+    }
+
+  def main(args: List[String]) = {
+    val rand = new Random
+    println(rand.nextInt())
+
+    val integers = new Generator[Int] {
+        val rand = new Random
+        def generate = rand.nextInt()
+      }
+
+    val booleans = for (x <- integers) yield x > 0
+
+    val ps = pairs(integers, integers)
+  }
 }
