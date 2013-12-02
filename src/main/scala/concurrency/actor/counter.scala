f7596afe60dc0d6761394c1b20120a91a0a2d561
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-02 19:42:45 +0100

    Tryout -> status fail!

--- src/main/scala/concurrency/actor/counter.scala
+++ src/main/scala/concurrency/actor/counter.scala
@@ -5,33 +5,42 @@ import akka.actor.Props
 
 class Counter extends Actor {
   var count = 0
+
   def receive = {
     case "incr" => count += 1
     case "get"  => sender ! count
   }
 }
 
-object CounterMain {
-  case object Done
-}
-
 class CounterMain extends Actor {
 
-  override def preStart(): Unit = {
-//    val counterMain = context.actorOf(Props[CounterMain], "countermain")
+  def start: Unit = {
     val counter = context.actorOf(Props[Counter], "counter")
 
     counter ! "incr"
     counter ! "incr"
     counter ! "get"
 
-    self ! CounterMain.Done
+    self ! "end"
   }
 
   def receive = {
-    case count: Int =>
-      println(s"count was $count")
-    case CounterMain.Done =>
-      context.stop(self)
+    case "start"    => start
+    case count: Int => println(s"count was $count")
+    case "end"      => context.stop(self)
   }
+}
+
+object CounterActorTryout {
+  import akka.actor.{Props, Actor, ActorSystem}
+
+  def main(args: Array[String]) = {
+    val system = ActorSystem("Tryout")
+
+    val c = system.actorOf(Props[CounterMain])
+
+    c ! "start"
+  }
+
+
 }
