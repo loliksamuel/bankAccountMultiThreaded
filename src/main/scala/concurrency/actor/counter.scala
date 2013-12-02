8f51a7c2ec16b94f17e89f210d188d1722a4cb96
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-02 20:17:13 +0100

    Add some log trace

--- src/main/scala/concurrency/actor/counter.scala
+++ src/main/scala/concurrency/actor/counter.scala
@@ -7,7 +7,9 @@ class Counter extends Actor {
   var count = 0
 
   def receive = {
-    case "incr" => count += 1
+    case "incr" =>
+      println("Received 'incr' message")
+      count += 1
     case "get"  => sender ! count
   }
 }
