fd43b4c1a39eea41beeda5d21da927cc82860a2b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-02 20:11:50 +0100

    CounterMain Works!

--- src/main/scala/concurrency/actor/counter.scala
+++ src/main/scala/concurrency/actor/counter.scala
@@ -5,6 +5,7 @@ import akka.actor.Props
 
 class Counter extends Actor {
   var count = 0
+
   def receive = {
     case "incr" => count += 1
     case "get"  => sender ! count
@@ -17,11 +18,22 @@ class CounterMain extends Actor {
 
   counter ! "incr"
   counter ! "incr"
+  counter ! "incr"
   counter ! "get"
 
-  context.stop(self)
-
   def receive = {
-    case count: Int => println(s"count was $count")
+    case count: Int =>
+      println(s"count was $count")
+      context.stop(self)
   }
 }
+
+// ╭─tony@dagobah(1,57,) 20:10:48 ~/repo/perso/scala-lab (master)
+// ╰─➤  sbt "run-main akka.Main concurrency.actor.CounterMain"
+// [info] Loading global plugins from /home/tony/.sbt/plugins
+// [info] Set current project to scala-lab (in build file:/home/tony/repo/perso/scala-lab/)
+// [info] Running akka.Main concurrency.actor.CounterMain
+// count was 3
+// [INFO] [12/02/2013 20:11:36.294] [Main-akka.actor.default-dispatcher-4] [akka://Main/user/app-terminator] application s
+// upervisor has terminated, shutting down
+// [success] Total time: 4 s, completed 2 déc. 2013 20:11:36
