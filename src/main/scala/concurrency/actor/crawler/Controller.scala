f3c746a5430d8ef058ca7c04a4650de056e7d9f2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-03 18:26:53 +0100

    Implement Controller in charge of url reception then delegates to Getter (builds them as needed) and reception the result and send the result back to the caller.

--- src/main/scala/concurrency/actor/crawler/Controller.scala
+++ src/main/scala/concurrency/actor/crawler/Controller.scala
@@ -1,5 +1,29 @@
 package concurrency.actor
 
+import akka.actor.{Actor, Props, ActorRef}
+
 object Controller {
   case class Check(link: String, depth: Int)
+  case class Result(cache: Set[String])
+}
+
+/** Class in charge of spawning Getter to retrieve url's content.
+  */
+class Controller extends Actor {
+  import Controller._
+
+  var cache = Set.empty[String]
+  var children = Set.empty[ActorRef]
+
+  def receive = {
+    case Check(url, depth) =>
+      if(!cache(url) && depth > 0) // spawn a new Getter in charge of retrieving the url's content
+        children += context.actorOf(Props(new Getter(url, depth - 1)))
+      cache += url
+    case Getter.Done       => //
+        children -= sender
+        if (children.isEmpty) context.parent ! Result(cache) // computation done, we send the result to the caller
+  }
+
+
 }
