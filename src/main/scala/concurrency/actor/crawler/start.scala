604f90d968fdb76d082e3c5f5314ada194c8dce9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-03 09:01:03 +0100

    WebClient implementation as a future.

--- src/main/scala/concurrency/actor/crawler/start.scala
+++ src/main/scala/concurrency/actor/crawler/start.scala
@@ -1,17 +1,27 @@
 package concurrency.actor
 
+import scala.concurrent.Future
 import com.ning.http.client.AsyncHttpClient
+import scala.concurrent.Promise
+import java.util.concurrent.Executor
 
 object WebClient {
   private val client = new AsyncHttpClient
 
   case class BadStatus(status: Int) extends RuntimeException
 
-  def get(url: String) = {
-    val response = client.prepareGet(url).execute().get
-    if (response.getStatusCode < 400)
-      response.getResponseBodyExcerpt(131072)
-    else
-      throw new BadStatus(response.getStatusCode)
+  def get(url: String)(implicit exec: Executor): Future[String]  = {
+    val f = client.prepareGet(url).execute()
+    val p = Promise[String]()
+    f.addListener(new Runnable {
+                    def run = {
+                      val response = f.get
+                      if (response.getStatusCode < 400)
+                        p.success(response.getResponseBodyExcerpt(131072))
+                      else
+                        p.failure(BadStatus(response.getStatusCode))
+                    }
+                  }, exec)
+    p.future
   }
 }
