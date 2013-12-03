2a6bea26c655f1d89f86c1ca2dcf4ba56eefc894
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-03 22:18:59 +0100

    Adapt the cache name to a more appropriate one

--- src/main/scala/concurrency/actor/crawler/Controller.scala
+++ src/main/scala/concurrency/actor/crawler/Controller.scala
@@ -5,29 +5,29 @@ import scala.concurrent.duration._
 
 object Controller {
   case class Check(link: String, depth: Int)
-  case class Result(cache: Set[String])
+  case class Result(links: Set[String])
 }
 
 /** Class in charge of spawning Getter to retrieve url's content.
   */
-class Controller extends Actor {
+class Controller extends Actor with ActorLogging {
   import Controller._
   import context.dispatcher
 
   context.system.scheduler.scheduleOnce(10.seconds, self, ReceiveTimeout)
 
-  var cache = Set.empty[String]
+  var cacheLinks = Set.empty[String]
   var children = Set.empty[ActorRef]
 
   def receive = {
     case Check(url, depth) =>
-      if(!cache(url) && depth > 0) // spawn a new Getter in charge of retrieving the url's content
       log.debug("{} checking {}", depth, url)
+      if(!cacheLinks(url) && depth > 0) // spawn a new Getter in charge of retrieving the url's content
         children += context.actorOf(Props(new Getter(url, depth - 1)))
-      cache += url
+      cacheLinks += url
     case Getter.Done       => //
       children -= sender
-      if (children.isEmpty) context.parent ! Result(cache) // computation done, we send the result to the caller
+      if (children.isEmpty) context.parent ! Result(cacheLinks) // computation done, we send the result to the caller
     case ReceiveTimeout    => // this way, we do not wait too long and be able to send a stop all to every Getter spawned
       children foreach(_ ! Getter.Abort)
   }
