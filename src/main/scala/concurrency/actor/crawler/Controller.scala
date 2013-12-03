6304f345cd0d8642f22059eddcaa89c4d6267dbd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-03 18:34:52 +0100

    Deal with exceeding timeout + properly set the ending up

--- src/main/scala/concurrency/actor/crawler/Controller.scala
+++ src/main/scala/concurrency/actor/crawler/Controller.scala
@@ -1,6 +1,7 @@
 package concurrency.actor
 
-import akka.actor.{Actor, Props, ActorRef}
+import akka.actor.{Actor, Props, ActorRef, ReceiveTimeout}
+import scala.concurrent.duration._
 
 object Controller {
   case class Check(link: String, depth: Int)
@@ -12,6 +13,8 @@ object Controller {
 class Controller extends Actor {
   import Controller._
 
+  context.setReceiveTimeout(10.seconds)// limit the waiting time by 10 seconds
+
   var cache = Set.empty[String]
   var children = Set.empty[ActorRef]
 
@@ -21,9 +24,9 @@ class Controller extends Actor {
         children += context.actorOf(Props(new Getter(url, depth - 1)))
       cache += url
     case Getter.Done       => //
-        children -= sender
-        if (children.isEmpty) context.parent ! Result(cache) // computation done, we send the result to the caller
+      children -= sender
+      if (children.isEmpty) context.parent ! Result(cache) // computation done, we send the result to the caller
+    case ReceiveTimeout    => // this way, we do not wait too long and be able to send a stop all to every Getter spawned
+      children foreach(_ ! Getter.Abort)
   }
-
-
 }
